793cfaf2b703a3d6b5a6064d80821568200039b2
felgro <felgro93@gmail.com>
2015-12-11 19:14:20 -0600

    Create Funcion_membresia.h

---
+++ Funcion_membresia.h
@@ -0,0 +1,16 @@
+// Funcion_membresia.h
+
+#ifndef _FUNCION_MEMBRESIA_h
+#define _FUNCION_MEMBRESIA_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
+float FME_Cerca(float Lectura1);
+float FME_Lejos(float Lectura1);
